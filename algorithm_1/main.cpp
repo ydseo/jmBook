d2251d2ef8fc9b9d58e0a8f9bdc4e49158caf7ff
ydseo <ydseo2015@gmail.com>
2016-04-26 14:23:25 +0900

    Initial Commit

---
+++ algorithm_1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  algorithm_1
+//
+//  Created by syd on 2016. 4. 26..
+//  Copyright © 2016년 ydseo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
